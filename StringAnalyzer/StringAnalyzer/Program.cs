ad9d98471b4fd3cee4cd9c4061052e70ed98f3b4
HansThePanzergrenadier <folyagamechannel@gmail.com>
2021-11-01 17:33:59 +0200

    TextAnalyzerAdded

---
+++ StringAnalyzer/StringAnalyzer/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace StringAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String filename;
+            char[] input;
+            bool pasteFlag = false, mergeFlag = true, percFlag = true, advancedOutputFlag = true;
+            Record.SortingMode sortingMode = Record.SortingMode.ByChar;
+            StreamReader reader;
+            List<Record> counted;
+
+
+            //if .exe is runned without batfile or console, we should determine filename manually
+            if (args.Length == 0)
+            {
+                do
+                {
+                    Console.WriteLine("Enter txt filename to analyze or: ");
+                    Console.WriteLine("   - enter 'q' to close program ");
+                    Console.WriteLine("   - enter 't' to paste complete text to analyze ");
+                    filename = Console.ReadLine();
+                    if (filename == "q")
+                    {
+                        return;
+                    }
+                    else if (filename == "t")
+                    {
+                        pasteFlag = true;
+                        break;
+                    }
+                }
+                while (!File.Exists(filename));
+
+                Console.WriteLine("Change options? y/n");
+                if(Console.ReadLine() == "y")
+                {
+                    Console.WriteLine("Enable merging upper and lower letters? y/n");
+                    if(Console.ReadLine() == "y")
+                    {
+                        mergeFlag = true;
+                    }
+                    else
+                    {
+                        mergeFlag = false;
+                    }
+
+                    Console.WriteLine("Show results in percentage? y/n");
+                    if (Console.ReadLine() == "y")
+                    {
+                        percFlag = true;
+                    }
+                    else
+                    {
+                        percFlag = false;
+                    }
+
+                    Console.WriteLine("Select sorting mode");
+                    Console.WriteLine("   - enter 'c' to sort by characters");
+                    Console.WriteLine("   - enter 'n' to sort by number");
+                    Console.WriteLine("   - enter 'x' to disable sorting");
+                    switch (Console.ReadLine())
+                    {
+                        case "c":
+                            sortingMode = Record.SortingMode.ByChar;
+                            break;
+                        case "n":
+                            sortingMode = Record.SortingMode.ByNumber;
+                            break;
+                        case "x":
+                            sortingMode = Record.SortingMode.None;
+                            break;
+                        default:
+                            break;
+                    }
+
+                    Console.WriteLine("Show advanced output? y/n");
+                    if(Console.ReadLine() == "y")
+                    {
+                        advancedOutputFlag = true;
+                    }
+                    else
+                    {
+                        advancedOutputFlag = false;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Program will run on default options");
+                }
+
+                if (!pasteFlag)
+                    Console.WriteLine("File found, beginning analysis...");
+            }
+            else
+            {
+                filename = args.ToString();
+            }
+
+            //we can paste whole text just in console as well
+            if (!pasteFlag)
+            {
+                reader = new StreamReader(filename);
+                input = reader.ReadToEnd().ToCharArray();
+            }
+            else
+            {
+                Console.WriteLine("Paste your text as a single line: ");
+                input = Console.ReadLine().ToCharArray();
+            }
+
+            //simply count each founded symbol
+            counted = Record.CountRecords(input);
+            if (mergeFlag)
+            {
+                Record.MergeSameLetters(counted);
+            }
+
+            switch (sortingMode)
+            {
+                case Record.SortingMode.ByChar:
+                    counted = Record.SortRecordsByChar(counted);
+                    break;
+                case Record.SortingMode.ByNumber:
+                    counted = Record.SortRecordsByNumber(counted);
+                    break;
+                case Record.SortingMode.None:
+                    //nothing to do
+                    break;
+                default:
+                    counted = Record.SortRecordsByChar(counted);
+                    break;
+            }
+
+            if (percFlag)
+            {
+                counted = Record.ConvertToPercentage(counted);
+            }
+
+            //display data
+            if (advancedOutputFlag)
+            {
+                Record.DrawGraphics(counted);
+            }
+            else
+            {
+                Record.DrawSimple(counted);
+            }
+
+            Console.ReadKey();
+        }
+
+        
+    }
+}
---
+++ StringAnalyzer/StringAnalyzer/Record.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAnalyzer
+{
+    class Record
+    {
+        public char Character;
+        public double Count;
+
+        public enum SortingMode
+        {
+            ByNumber,
+            ByChar,
+            None
+        }
+
+        public Record(char Character, double Count)
+        {
+            this.Character = Character;
+            this.Count = Count;
+        }
+
+        public static void DrawSimple(SortedDictionary<char, int> counted)
+        {
+            foreach (var el in counted)
+            {
+                Console.WriteLine("{0} - {1}", el.Key, el.Value);
+            }
+        }
+
+        public static void DrawSimple(List<Record> counted)
+        {
+            foreach (var el in counted)
+            {
+                Console.WriteLine("{0} - {1}", el.Character, el.Count);
+            }
+        }
+
+        public static SortedDictionary<char, int> CountSortedDict(char[] input)
+        {
+            SortedDictionary<char, int> counted = new SortedDictionary<char, int>();
+
+            foreach (char a in input)
+            {
+                if (!counted.ContainsKey(a))
+                {
+                    int count = 0;
+                    foreach (char b in input)
+                    {
+                        if (a.Equals(b))
+                        {
+                            count++;
+                        }
+                    }
+
+                    counted.Add(a, count);
+                }
+            }
+
+            return counted;
+        }
+
+        public static List<Record> CountRecords(char[] text)
+        {
+            List<Record> recs = new List<Record>();
+            for (int i = 0; i < text.Length; i++)
+            {
+                char ch = text[i];
+                int count = 0;
+                for (int j = 0; j < text.Length; j++)
+                {
+                    if (ch.Equals(text[j]))
+                    {
+                        count++;
+                    }
+                }
+
+                bool existsFlag = false;
+                for (int j = 0; j < recs.Count; j++)
+                {
+                    Record el = recs[j];
+                    if (el.Character.Equals(ch))
+                    {
+                        existsFlag = true;
+                        break;
+                    }
+                }
+
+                if (!existsFlag)
+                {
+                    recs.Add(new Record(ch, count));
+                }
+            }
+            return recs;
+        }
+
+        public static List<Record> SortRecordsByChar(List<Record> input)
+        {
+            List<Record> recs = input;
+            if (recs.Count > 0)
+            {
+                List<Record> result = new List<Record>();
+                while (recs.Count > 0)
+                {
+                    Record max = recs[0];
+                    for (int i = 0; i < recs.Count; i++)
+                    {
+                        if (recs[i].Character > max.Character)
+                        {
+                            max = recs[i];
+                        }
+                    }
+                    recs.Remove(max);
+                    result.Add(max);
+                }
+                result.Reverse();
+                return result;
+            }
+            else
+            {
+                throw new Exception("list is empty");
+            }
+        }
+
+        public static List<Record> SortRecordsByNumber(List<Record> input)
+        {
+            List<Record> recs = input;
+            if (recs.Count > 0)
+            {
+                List<Record> result = new List<Record>();
+                while (recs.Count > 0)
+                {
+                    Record max = recs[0];
+                    for (int i = 0; i < recs.Count; i++)
+                    {
+                        if (recs[i].Count > max.Count)
+                        {
+                            max = recs[i];
+                        }
+                    }
+                    recs.Remove(max);
+                    result.Add(max);
+                }
+                return result;
+            }
+            else
+            {
+                throw new Exception("list is empty");
+            }
+        }
+
+        public static void MergeSameLetters(List<Record> recs)
+        {
+            if (recs.Count > 0)
+            {
+                for (int i = 0; i < recs.Count; i++)
+                {
+                    Record a = recs[i];
+                    char aC = a.Character;
+                    if (Char.IsLetter(aC) && Char.IsUpper(aC))
+                    {
+                        for (int j = 0; j < recs.Count; j++)
+                        {
+                            Record b = recs[j];
+                            Char bC = b.Character;
+                            if (Char.IsLetter(bC) && Char.IsLower(bC) && Char.ToUpper(bC).Equals(aC))
+                            {
+                                a.Count += b.Count;
+                                recs.Remove(b);
+                            }
+                        }
+                    }
+                }
+                foreach(Record el in recs)
+                {
+                    if (Char.IsUpper(el.Character))
+                    {
+                        el.Character = Char.ToLower(el.Character);
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception("list is empty");
+            }
+        }
+
+        public static List<Record> ConvertToPercentage(List<Record> recs)
+        {
+            List<Record> result = new List<Record>();
+            double total = 0;
+            foreach (Record el in recs)
+            {
+                total += el.Count;
+            }
+            foreach(Record el in recs)
+            {
+                result.Add(new Record(el.Character, (el.Count / total) * 100));
+            }
+            return result;
+        }
+
+        public static void DrawGraphics(List<Record> recs)
+        {
+            foreach (Record el in recs)
+            {
+                Console.Write(el.Character);
+                Console.CursorLeft = 3;
+                Console.Write("| {0:F2}", el.Count);
+                Console.CursorLeft = 10;
+                Console.Write("| ");
+                for (int i = 0; i < el.Count; i++)
+                {
+                    Console.Write("█");
+                }
+                
+                Console.WriteLine();
+            }
+        }
+    }
+}
